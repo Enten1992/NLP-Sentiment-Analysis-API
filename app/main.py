ca452b9c1be69e2cf11c5596d9b58bd4053a83f6
Ethan Reed <grifacspirdeapath@gmx.net>
2023-04-18 00:00:00 -0400

    Initial code commit

--- app/main.py
+++ app/main.py
@@ -1,41 +1,47 @@
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
-import logging
+import os
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format=\"%(asctime)s - %(levelname)s - %(message)s\")
+# Initialize FastAPI app
+app = FastAPI(title="NLP Sentiment Analysis API",
+              description="A simple API for performing sentiment analysis on text.",
+              version="1.0.0")
 
-app = FastAPI(
-    title=\"Sentiment Analysis API\",
-    description=\"A simple API for sentiment analysis using Hugging Face Transformers.\",
-    version=\"1.0.0\"
-)
+# Load sentiment analysis pipeline
+# You can specify a different model if needed, e.g., "distilbert-base-uncased-finetuned-sst-2-english"
+# Ensure the model is downloaded or available offline for production deployment
+model_name = os.getenv("SENTIMENT_MODEL", "distilbert-base-uncased-finetuned-sst-2-english")
+print(f"Loading sentiment analysis model: {model_name}...")
+sentiment_pipeline = pipeline("sentiment-analysis", model=model_name)
+print("Sentiment analysis model loaded successfully.")
 
-# Load pre-trained sentiment analysis model
-# Using a smaller, fine-tuned BERT model for efficiency
-logging.info("Loading sentiment analysis model...")
-sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-logging.info("Sentiment analysis model loaded successfully.")
-
-class TextInput(BaseModel:
+class TextInput(BaseModel):
     text: str
 
-@app.post("/sentiment")
-async def analyze_sentiment(text_input: TextInput):
+@app.get("/", summary="Root endpoint", tags=["Health Check"])
+async def read_root():
+    return {"message": "Welcome to the NLP Sentiment Analysis API! Visit /docs for API documentation.",
+            "status": "healthy"}
+
+@app.post("/analyze_sentiment", summary="Analyze sentiment of text", tags=["Sentiment Analysis"])
+async def analyze_sentiment(input: TextInput):
     """
     Analyzes the sentiment of the provided text.
-    """
-    logging.info(f"Received sentiment analysis request for text: {text_input.text[:50]}...")
-    result = sentiment_pipeline(text_input.text)[0]
-    sentiment = result["label"]
-    score = result["score"]
-    logging.info(f"Sentiment analysis complete. Label: {sentiment}, Score: {score:.4f}")
-    return {"text": text_input.text, "sentiment": sentiment, "score": score}
 
-@app.get("/health")
-async def health_check():
+    - **text**: The input text string to analyze.
     """
-    Health check endpoint to verify API status.
-    """
-    return {"status": "ok", "model_loaded": True}
+    try:
+        result = sentiment_pipeline(input.text)
+        return {"input_text": input.text, "sentiment_result": result[0]}
+    except Exception as e:
+        return {"error": str(e), "message": "Failed to analyze sentiment."}
+
+if __name__ == "__main__":
+    import uvicorn
+    # To run this application:
+    # uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+    # Or directly via Python script (for development/testing):
+    print("Starting Uvicorn server...")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
